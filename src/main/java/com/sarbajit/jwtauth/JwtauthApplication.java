0ca5ad2d7da645a4f17c2e115627d05c676d670b
smohantyargus <smohanty@argusoft.com>
2023-10-04 10:52:16 +0530

    role based auth

--- src/main/java/com/sarbajit/jwtauth/JwtauthApplication.java
+++ src/main/java/com/sarbajit/jwtauth/JwtauthApplication.java
@@ -1,7 +1,12 @@
 package com.sarbajit.jwtauth;
 
+import com.sarbajit.jwtauth.auth.AuthenticationService;
+import com.sarbajit.jwtauth.auth.RegisterRequest;
+import com.sarbajit.jwtauth.user.Role;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JwtauthApplication {
@@ -11,4 +16,26 @@ public class JwtauthApplication {
 		System.out.println("API RUNNING ON PORT: 8080");
 	}
 
+//	@Bean
+//	public CommandLineRunner commandLineRunner(AuthenticationService authenticationService){
+//		return args -> {
+//			var admin = RegisterRequest.builder()
+//					.firstname("Admin")
+//					.lastname("Admin")
+//					.email("admin@admin.com")
+//					.password("password")
+//					.role(Role.ADMIN)
+//					.build();
+//			System.out.println("ADMIN TOKEN: "+ authenticationService.register(admin).getAccessToken());
+//
+//			var manager = RegisterRequest.builder()
+//					.firstname("Manager")
+//					.lastname("Manager")
+//					.email("manager@manager.com")
+//					.password("password")
+//					.role(Role.MANAGER)
+//					.build();
+//			System.out.println("MANAGER TOKEN: "+ authenticationService.register(manager).getAccessToken());
+//		};
+//	}
 }
--- src/main/java/com/sarbajit/jwtauth/config/SecurityConfiguration.java
+++ src/main/java/com/sarbajit/jwtauth/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +13,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+import static com.sarbajit.jwtauth.user.Permission.*;
+import static com.sarbajit.jwtauth.user.Role.ADMIN;
+import static com.sarbajit.jwtauth.user.Role.MANAGER;
+import static org.springframework.http.HttpMethod.*;
 
 @Configuration
 @EnableWebSecurity
@@ -28,8 +34,44 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
+
                 .requestMatchers("/api/v1/auth/**")
                 .permitAll()
+
+
+                .requestMatchers("/api/v1/management/**")
+                .hasAnyRole(ADMIN.name(), MANAGER.name())
+
+                .requestMatchers(GET, "/api/v1/management/**")
+                .hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name())
+
+                .requestMatchers(POST, "/api/v1/management/**")
+                .hasAnyAuthority(ADMIN_CREATE.name(), MANAGER_CREATE.name())
+
+                .requestMatchers(PUT, "/api/v1/management/**")
+                .hasAnyAuthority(ADMIN_UPDATE.name(), MANAGER_UPDATE.name())
+
+                .requestMatchers(DELETE, "/api/v1/management/**")
+                .hasAnyAuthority(ADMIN_DELETE.name(), MANAGER_DELETE.name())
+
+
+
+                .requestMatchers("/api/v1/admin/**")
+                .hasRole(ADMIN.name())
+
+                .requestMatchers(GET, "/api/v1/admin/**")
+                .hasAuthority(ADMIN_READ.name())
+
+                .requestMatchers(POST, "/api/v1/admin/**")
+                .hasAuthority(ADMIN_CREATE.name())
+
+                .requestMatchers(PUT, "/api/v1/admin/**")
+                .hasAuthority(ADMIN_UPDATE.name())
+
+                .requestMatchers(DELETE, "/api/v1/admin/**")
+                .hasAuthority(ADMIN_DELETE.name())
+
+
                 .anyRequest()
                 .authenticated()
                 .and()
